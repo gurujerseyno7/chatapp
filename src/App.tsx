a0641b5343d4c6ec4a83e6b097d0cf20daa04230
Aakash-Sivakumar <aakashash9@gmail.com>
2025-02-19 10:55:34 +0530

    removed test data from app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-
 import './App.css';
-import CustomTextField from './components/customtextfield/CustomTextField';
-
 
 function App() {
   return (
     <div>
       Hi Team!
-      <CustomTextField label={'Name'} placeholder={'AshKetchum'} variant={'filled'} type={'search'} helperText={''} name={''} className={''} isOptional={false}/>
     </div>
   );
 }
